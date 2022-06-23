2498faaefd21949aee2c5ff0b269deb44228736a
Kirill Key <32145269+kirillnik25@users.noreply.github.com>
2022-06-24 02:16:18 +0300

    fix pipeline

--- pipeline/.ipynb_checkpoints/pipeline-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from dagster import repository, get_dagster_logger
-from jobs.hn_launches_job import hn_launches_job
-from jobs.growjo_job import growjo_job
-
-
-@repository
-def hn_launches():
-    
-    return [hn_launches_job, growjo_job]
