36a1c344a24a986a92bea1c2d32d41bb1fc6ded5
Kirill Key <32145269+kirillnik25@users.noreply.github.com>
2022-06-24 19:42:21 +0300

    dagster: fixes

--- pipeline/ops/parse_growjo.py
+++ pipeline/ops/parse_growjo.py
@@ -14,29 +14,28 @@ from utils.utils import *
 from dagster import op, Out, In
 
 
-with open('/opt/dagster/app/ops/config.yaml') as c: 
+with open('/opt/dagster/app/ops/config.yaml') as c:
     config = yaml.load(c, Loader = SafeLoader)
-    
+
 
 @op(description = 'Parse companies growth metrics from Gowjo')
 def parse_growjo(context) -> pd.core.frame.DataFrame:
-    
+
     clickhouse = Client(host = config['clickhouse']['host'], database = 'hn_launches',
                         user = config['clickhouse']['user'], password = config['clickhouse']['password'])
-    
+
     launches = clickhouse.query_dataframe('select distinct item_id, name, urls, is_oss from hn_launches.launches;')
-    ch_company_growth = ('select distinct item_id from hn_launches.company_growth;')
     
-    
+
     #[Parse Growjo companies metrics]
-    company_growth = pd.DataFrame(columns = ['item_id', 'founded', 'url', 'linkedin_url', 'country',  
+    company_growth = pd.DataFrame(columns = ['item_id', 'founded', 'url', 'linkedin_url', 'country',
                                              'employees', 'estimated_revenue', 'total_funding'])
 
     for name in launches['name'].unique():
 
         time.sleep(np.random.poisson(5))
 
-        response = r.get(f"https://growjo.com/api/companies/{name.replace(' ', '_')}", 
+        response = r.get(f"https://growjo.com/api/companies/{name.replace(' ', '_')}",
                          headers = {'authority': 'growjo.com', \
          'accept': 'application/json, text/plain, */*', \
          'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', \
@@ -56,20 +55,20 @@ def parse_growjo(context) -> pd.core.frame.DataFrame:
                 response = json.loads(response.text)
 
                 company_growth = company_growth.append({
-                    'item_id': launches[launches['name'] == name]['item_id'], 
+                    'item_id': launches[launches['name'] == name]['item_id'],
                     'founded': response['founded'],
-                    'url':response['url'], 
-                    'linkedin_url': response['linkedin_url'], 
-                    'country': response['country'], 
-                    'employees': response['current_employees'], 
-                    'estimated_revenue': response['estimated_revenues'], 
+                    'url':response['url'],
+                    'linkedin_url': response['linkedin_url'],
+                    'country': response['country'],
+                    'employees': response['current_employees'],
+                    'estimated_revenue': response['estimated_revenues'],
                     'total_funding': response['total_funding_float']},
                     ignore_index = True)
 
         company_growth.append(company_growth)
 
     company_growth['item_id'] = company_growth['item_id'].astype('int')
-    
+
     context.log.info(f"Parsed {len(company_growth['item_id'])} Growjo companies")
-    
+
     return company_growth
--- pipeline/ops/transform_load_growth.py
+++ pipeline/ops/transform_load_growth.py
@@ -14,7 +14,7 @@ from utils.utils import *
 from dagster import op, Out, In
 
 
-with open('/opt/dagster/app/ops/config.yaml') as c: 
+with open('/opt/dagster/app/ops/config.yaml') as c:
     config = yaml.load(c, Loader = SafeLoader)
 
 
@@ -24,20 +24,19 @@ def transform_load_growth(context, company_growth:pd.core.frame.DataFrame, compa
     clickhouse = Client(host = config['clickhouse']['host'], database = 'hn_launches',
                         user = config['clickhouse']['user'], password = config['clickhouse']['password'])
 
-    ch_company_growth = ('select distinct item_id from hn_launches.company_growth;')
+    ch_company_growth = clickhouse.query_dataframe('select distinct item_id from hn_launches.company_growth;')
 
     company_growth = pd.merge(company_growth, company_oss, how = 'outer', on = 'item_id').replace(np.nan, 0)
-    
+
     company_growth = company_growth.astype({'founded': 'int', 'employees': 'int', 'estimated_revenue': 'int', 'total_funding': 'float',
                                             'github_stars': 'int', 'github_forks': 'int', 'github_open_issues': 'int'})
-    
-    
+
+
     if len(company_growth['item_id']) >= len(ch_company_growth['item_id']):
 
         clickhouse.execute('alter table hn_launches.company_growth delete where 1=1;')
-        
+
         rows = clickhouse.execute('insert into hn_launches.company_growth values',
                                   company_growth.to_dict('records'), types_check = True)
-        
-        context.log.info(f"Companies growth loaded to CH, inserted rows: {rows}")
 
+        context.log.info(f"Companies growth loaded to CH, inserted rows: {rows}")
